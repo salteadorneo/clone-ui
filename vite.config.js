faafe971908745bf2c3d53b70aa5ebb6ff8b4501
Cristian Adán <cristian@salteadorneo.dev>
2026-05-28 20:27:54 +0200

    remove Vercel insights script and update base URL in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://salteadorneo.github.io/clone-ui/',
+  base: 'https://salteadorneo.github.io/',
   plugins: [react()]
 })
