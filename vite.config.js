e910217ce17f518fd67cd5a53d026f7cf3183bdb
Cristian Adán <cristian@salteadorneo.dev>
2026-05-28 20:36:12 +0200

    update Vite config: set site URL and adjust base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: 'https://salteadorneo.github.io/',
+  site: 'https://salteadorneo.github.io',
+  base: '/clone-ui',
   plugins: [react()]
 })
